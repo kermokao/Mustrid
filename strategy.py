b0690a6575ee08b03c6ff988618612b0bc9ffd10
kermokao <kermo.kao@voco.ee>
2026-04-08 11:21:18 +0300

    Create strategy.py

---
+++ strategy.py
@@ -0,0 +1,42 @@
+import math
+
+class DiscriminantStrategy:
+    def calculate_discriminant(self, a, b, c):
+        pass
+
+
+class OrdinaryDiscriminantStrategy(DiscriminantStrategy):
+    def calculate_discriminant(self, a, b, c):
+        return b**2 - 4*a*c
+
+
+class RealDiscriminantStrategy(DiscriminantStrategy):
+    def calculate_discriminant(self, a, b, c):
+        d = b**2 - 4*a*c
+        if d < 0:
+            return float("Nan")
+
+
+class QuadraticEquationSolver:
+    def __init__(self, strategy):
+        self.strategy = strategy
+
+    def solve(self, a, b, c):
+
+       d = self.strategy.calculate_discriminant(a, b, c)
+       sqrt_d = math.sqrt (d)
+
+
+       x1 = (b - sqrt_d) / (2 * a)
+       x2 = (b + sqrt_d) / (2 * a)
+       
+       return (x1, x2)
+
+solver = QuadraticEquationSolver(OrdinaryDiscriminantStrategy())
+print(solver.solve(1, 10, 16))
+
+solver = QuadraticEquationSolver(RealDiscriminantStrategy())
+print(solver.solve(1, 4, 5))
+
+       
+
